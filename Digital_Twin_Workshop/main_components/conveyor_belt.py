87fdb1121f246ba2433090713dd574df6bb85b6a
jakobtitz <titz.jakob@gmail.com>
2025-11-19 15:41:57 +0000

    last minute fix

--- Digital_Twin_Workshop/main_components/conveyor_belt.py
+++ Digital_Twin_Workshop/main_components/conveyor_belt.py
@@ -15,6 +15,8 @@ class ConveyorBelt(BaseMachine):
     # - repr and todict (change attribute names)
     
     def __init__(self, machine_name=None):
+        super().__init__(machine_name)
+        self.attr1 = 0
         pass
     
     def process_mqtt(self, attribute_name, value):
@@ -26,10 +28,8 @@ class ConveyorBelt(BaseMachine):
         data = super().to_dict()
         data.update({
             "attr1": self.attr1,
-            "attr2": self.attr2
         })
         return data
     
     def __repr__(self):
-        return (f"ConveyorBelt({self.machine_name}, attr1={self.attr1}, "
-                f"attr2={self.attr2})")
+        return (f"ConveyorBelt({self.machine_name}, attr1={self.attr1},)")
